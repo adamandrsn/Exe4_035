f4d0f5d4dd42be1c35e1f4907229bf65f14538de
Adam Anderson <adam.anderson.ft21@mail.umy.ac.id>
2022-12-07 15:42:36 +0700

    add class Node

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+
 
 namespace Exercise4
 {
-    internal class Program
+    class Node
     {
-        static void Main(string[] args)
+        public int info;
+        public Node next;
+        public Node(int i, Node n)
         {
+            info = i;
+            next = n;
         }
     }
 }
