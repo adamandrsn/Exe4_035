727dae151866112a95fde8f8525127633b03a694
Adam Anderson <adam.anderson.ft21@mail.umy.ac.id>
2022-12-07 15:45:28 +0700

    add method public void pop

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -41,5 +41,10 @@ namespace Exercise4
             top = fresh;
             Console.WriteLine("\n" + element + "pushed");
         }
+        public void pop()
+        {
+            Console.WriteLine("\nThe popped element is: " + top.info);
+            top = top.next; //Make top point to the next node in sequence
+        }
     }
 }
