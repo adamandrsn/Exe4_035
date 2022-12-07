1571128982343d549412322e93699b5387fd8f36
Adam Anderson <adam.anderson.ft21@mail.umy.ac.id>
2022-12-07 15:49:02 +0700

    add console.writeline

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -65,7 +65,15 @@ namespace Exercise4
         {
             Stacks s = new Stacks();
             while (true)
-            { }
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("\nEnter your choice: ");
+            }
         }
     }
 }
