971023853784be59ab5e381a667d1f77da0aa1c6
Adam Anderson <adam.anderson.ft21@mail.umy.ac.id>
2022-12-07 15:45:05 +0700

    add method public void push

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -32,5 +32,14 @@ namespace Exercise4
             else
                 return (false);
         }
+        public void push(int element)
+        {
+            Node fresh;
+            fresh = new Node(element, null);
+
+            fresh.next = top;
+            top = fresh;
+            Console.WriteLine("\n" + element + "pushed");
+        }
     }
 }
